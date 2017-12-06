bd02772f3f3da17aab330fc44eec1c114b38c249
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2017-12-06 12:51:04 -0500

    More LInkedList work

--- 20_LinkedLists_Starter/src/LinkedList.java
+++ 20_LinkedLists_Starter/src/LinkedList.java
@@ -46,7 +46,13 @@ public class LinkedList {
 	// removes the first element in the list,
 	//  returning the data therein
 	public Object removeFirst(){
-		// TODO: complete this method
+		Object data = getFirst();
+		//left hand side: the arrow reference to change
+		//right hand side: where it shold point
+		//see 12-1-17 notes for the node graph list thingy at the very top
+		first = first.next;
+				
+		return data;
 	}
 
 
@@ -57,6 +63,7 @@ public class LinkedList {
 
 
 	class LinkedListIterator implements ListIterator {
+
 		// The most recent node visited, null if none yet
 		private Node position;
 
@@ -70,11 +77,36 @@ public class LinkedList {
 
 		public LinkedListIterator(){
 			// TODO: complete this constructor with the instructor
+			position = null;
+			previous = null;
+			isAfterNext = false;
 		}
 
 		@Override
 		public boolean hasNext() {
 			// TODO: complete this method with the instructor
+			//what's keeping track of my current position?
+			
+			//if we crank up a new iterator, and the position is null, and we call .next(),
+			//it'll crash
+			if (position == null) {
+				
+				//nothing visited yet; check if there is a first item
+				if (first == null) {
+					return false;
+				} else {
+					return true;
+				}
+				
+				//OR REALLY CLEAN CODE: return (first != null);
+			}
+			if (position.next == null) {
+				//position is the last thing
+				return false;
+			} else {
+				return true;
+			}
+			//REALLY CLEAN CODE: return position.next != null;
 		}
 
 		@Override
@@ -82,7 +114,18 @@ public class LinkedList {
 			if(!hasNext()){
 				throw new NoSuchElementException();
 			}
+			previous = position;
+			if (position == null) {
+				position = first;
+			} else {
+				position = position.next;
+			}
 			
+			//REALLY CLEAN CODE: position = (position == null ? first : position.next);
+			
+			isAfterNext = true;
+			
+			return position.data;
 			// TODO: complete this method
 			
 			// NOTE: Which member variables need to change?
@@ -92,14 +135,35 @@ public class LinkedList {
 		@Override
 		public void set(Object element) {
 			// TODO: complete this method with the instructor
+			if (!isAfterNext) {
+				throw new IllegalStateException();
+			}
+			
+			position.data = element;
 		}
 
 		@Override
 		public void remove() {
 			// NOTE: What are the different cases to consider?
 			//       Don't worry about what to do for them yet, just list the cases
-
-			// TODO: complete this method with the instructor
+			
+			//If position is not valid, or isAfterNext is false (Previous is not valid)
+			// remove something from the middle of the list
+			//				"				start		"
+			//				"				end			"
+			
+			if (!isAfterNext) {
+				throw new IllegalStateException();
+			}
+			
+			if (position == first) {
+				position = null;
+				removeFirst();
+			} else {
+				position = previous;
+				previous = null;
+			}
+			isAfterNext = false;
 		}
 
 		
@@ -114,10 +178,31 @@ public class LinkedList {
 				addFirst(element);
 				position = first;
 			}
-			else{
+			else {
 				// TODO: complete this part in groups
 				// NOTE: I recommend drawing a diagram to figure out what references need to be altered
-
+				
+				/*
+				Node node = new Node();
+				node.next = position.next;
+				position.next = node;
+				node.data = element;
+				MITCHELL */
+				
+				//make the new node and set its data
+				Node newNode = new Node();
+				newNode.data = element;
+				
+				//place the node in the list
+				newNode.next = position.next;
+				position.next = newNode;
+				//the right hand is where am I pointing at
+				//the left hand is what am I pointing with
+				
+				//move position of ListIterator so you get Romeo and not Juliet
+				//aka, advance the iterator
+				position = newNode;
+				//or position = postition.next;
 			}
 			
 			isAfterNext = false;
@@ -130,11 +215,30 @@ public class LinkedList {
 
 	public int size(){
 		// iterative version
-		// TODO: complete this with the instructor
+		int count = 0;
+		ListIterator iter = iterator();
+		
+		while(iter.hasNext()) {
+			count++;
+			iter.next();
+		}
+		return count;
 	}
 	
 	public int size2(){
 		// recursive version
-		// TODO: complete this with the instructor
+		return sizeRecursive(first);		
+	}
+	
+	//we need some argument to keep track of where we are for the recursion
+	//returns the number of NDOes in the list starting at n
+	private int sizeRecursive(Node n) {
+		if (n == null) {
+			//base case
+			return 0;
+		} else {
+			return 1 + sizeRecursive(n.next);
+			//how many nodes are there from n onward?
+		}
 	}
 }
