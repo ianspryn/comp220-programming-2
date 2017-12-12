bc037f4e758e5b77602a532710cc5e60e9b9b6e4
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2017-12-11 23:25:26 -0500

    Finish Unordered Pair

--- 21_3_UnorderedPair_Starter/src/UnorderedPair.java
+++ 21_3_UnorderedPair_Starter/src/UnorderedPair.java
@@ -1,2 +1,38 @@
 
-// TODO: define your UnorderedPair class here
+public class UnorderedPair<T> {
+	private T t, t2;
+	
+	public UnorderedPair(T first, T second) {
+		this.t = first;
+		this.t2 = second;
+	}
+	
+	public boolean contains(T type) {
+		return type.equals(t) || type.equals(t2);
+	}
+	
+	@Override public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!(obj instanceof UnorderedPair)){
+			return false;
+		}
+		
+		UnorderedPair<?> other = (UnorderedPair<?>) obj;
+		if((this.t.equals(other.t) && this.t2.equals(other.t2)) ||
+				(this.t.equals(other.t2) && this.t2.equals(other.t))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	@Override public int hashCode() {
+		int result = 17;
+		result = t.hashCode() * t2.hashCode();
+		
+		return result;
+	}
+}
